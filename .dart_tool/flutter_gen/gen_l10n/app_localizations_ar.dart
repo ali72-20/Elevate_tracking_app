cbab8103dc69b6633eb485f5da9740123d3f0cfe
alisafwat <alisafwat11@gmail.com>
2024-12-26 00:45:51 +0200

    update local

--- .dart_tool/flutter_gen/gen_l10n/app_localizations_ar.dart
+++ .dart_tool/flutter_gen/gen_l10n/app_localizations_ar.dart
@@ -6,5 +6,348 @@ import 'app_localizations.dart';
 class AppLocalizationsAr extends AppLocalizations {
   AppLocalizationsAr([String locale = 'ar']) : super(locale);
 
+  @override
+  String get connectionTimeout => 'انتهت مهلة الاتصال. يرجى التحقق من اتصال الإنترنت والمحاولة مرة أخرى.';
 
+  @override
+  String get sendTimeout => 'انتهت مهلة إرسال البيانات. يرجى المحاولة مرة أخرى لاحقًا.';
+
+  @override
+  String get receiveTimeout => 'انتهت مهلة استقبال البيانات. يرجى التحقق من الاتصال الخاص بك.';
+
+  @override
+  String get badCertificate => 'شهادة الخادم غير موثوقة. يرجى التحقق من أمان الشبكة.';
+
+  @override
+  String get connectionError => 'فشل الاتصال. يرجى التحقق من اتصال الشبكة الخاص بك.';
+
+  @override
+  String get unknown => 'حدث خطأ غير معروف. يرجى المحاولة مرة أخرى.';
+
+  @override
+  String get badRequest => 'طلب غير صالح. يرجى التحقق من البيانات والمحاولة مرة أخرى.';
+
+  @override
+  String get unauthorized => 'دخول غير مصرح به. يرجى تسجيل الدخول والمحاولة مرة أخرى.';
+
+  @override
+  String get conflict => 'تم اكتشاف تعارض. المورد موجود بالفعل أو يتعارض مع الحالة الحالية.';
+
+  @override
+  String get notFount => 'المورد غير موجود. العنصر المطلوب غير موجود.';
+
+  @override
+  String get internalServerError => 'خطأ في الخادم الداخلي. يرجى المحاولة مرة أخرى لاحقًا.';
+
+  @override
+  String get helloWorld => 'مرحبا';
+
+  @override
+  String get login => 'تسجيل الدخول';
+
+  @override
+  String get email => 'البريد الالكتروني';
+
+  @override
+  String get password => 'كلمة السر';
+
+  @override
+  String get enterYourEmail => 'ادخل البريد الالكتروني';
+
+  @override
+  String get enterYourPassword => 'ادخل كلمة السر';
+
+  @override
+  String get notValidEmail => 'البزيد الالكتروني غير متاح';
+
+  @override
+  String get rememberMe => 'تذكرني';
+
+  @override
+  String get forgetPassword => 'نسيت كلمة السر؟';
+
+  @override
+  String get continueAsGuest => 'الاستمرار كضيف';
+
+  @override
+  String get dontHaveAccount => 'لا تمتلك حساب؟ ';
+
+  @override
+  String get signUp => 'انشاء حساب';
+
+  @override
+  String get emailNotValid => 'هذا البريد غير صالح';
+
+  @override
+  String get categories => 'الاقسام';
+
+  @override
+  String get best_Seller => 'الاكثر مبيعا';
+
+  @override
+  String get occasions => 'المناسبات';
+
+  @override
+  String get search => 'بحث';
+
+  @override
+  String get noProducts => 'لا يوجد منتجات متاحة';
+
+  @override
+  String get somethingWentWrong => 'حدث خطأ ما';
+
+  @override
+  String get giftNow => 'قدم هدية';
+
+  @override
+  String get discoverNew => 'اكتشف شيئا جديدا';
+
+  @override
+  String get invalidPassword => 'كلمة سر غير صالحة';
+
+  @override
+  String get addToCart => 'اضف الى السلة';
+
+  @override
+  String get description => 'الوصف';
+
+  @override
+  String get allPricesIncludeTax => 'جميع الاسعار تشمل الضريبة';
+
+  @override
+  String get bouquetInclude => 'الباقة تشمل';
+
+  @override
+  String get egp => 'جنيه مصري';
+
+  @override
+  String get statue => 'الحالة';
+
+  @override
+  String get viewAll => 'عرض الكل';
+
+  @override
+  String get bestSeller => 'الأكثر مبيعًا';
+
+  @override
+  String get notification => 'الإشعارات';
+
+  @override
+  String get language => 'اللغة';
+
+  @override
+  String get aboutUs => 'معلومات عنا';
+
+  @override
+  String get termsAndConditions => 'الشروط والأحكام';
+
+  @override
+  String get myOrders => 'طلباتي';
+
+  @override
+  String get savedAddresses => 'العناوين المحفوظة';
+
+  @override
+  String get logout => 'تسجيل الخروج';
+
+  @override
+  String get logOut => 'تسجيل الخروج';
+
+  @override
+  String get bloomWithOurExquisiteBestSellers => 'ازدهر مع منتجاتنا الأكثر تميزًا';
+
+  @override
+  String get english => 'الإنجليزية';
+
+  @override
+  String get changePassword => 'تغيير كلمة السر';
+
+  @override
+  String get success => 'نجاح';
+
+  @override
+  String get profileUpdated => 'تم تحديث الملف الشخصي بنجاح';
+
+  @override
+  String get enterYourFirstName => 'ادخل اسمك الاول';
+
+  @override
+  String get enterYourLastName => 'ادخل اسمك الاخير';
+
+  @override
+  String get enterYourPhone => 'ادخل رقم هاتفك';
+
+  @override
+  String get firstName => 'الاسم الاول';
+
+  @override
+  String get lastName => 'الاسم الاخير';
+
+  @override
+  String get phone => 'رقم الهاتف';
+
+  @override
+  String get phoneError => 'يجب أن يكون رقم الهاتف على الأقل 10 أحرف';
+
+  @override
+  String get occasion => 'مناسبة';
+
+  @override
+  String get confirmLogout => 'هل أنت متأكد أنك تريد تسجيل الخروج؟';
+
+  @override
+  String get resetPassword => 'إعادة تعيين كلمة المرور';
+
+  @override
+  String get currentPassword => 'كلمة المرور الحالية';
+
+  @override
+  String get newPassword => 'كلمة المرور الجديدة';
+
+  @override
+  String get confirmPassword => 'تأكيد كلمة المرور';
+
+  @override
+  String get passwordNotMatch => 'كلمة المرور غير متطابقة';
+
+  @override
+  String get passwordChangedSuccessfully => 'تم تغيير كلمة المرور بنجاح';
+
+  @override
+  String get passwordNotChanged => 'لم يتم تغيير كلمة المرور';
+
+  @override
+  String get passwordChangedFailed => 'فشل تغيير كلمة المرور';
+
+  @override
+  String get update => 'تحديث';
+
+  @override
+  String get cartAddedSuccessfully => 'تمت إضافة المنتج إلى السلة بنجاح';
+
+  @override
+  String get error => 'Error';
+
+  @override
+  String get unauthorizedAccessPleaseLogin => 'الوصول غير مصرح به. يرجى تسجيل الدخول';
+
+  @override
+  String get addressDeleted => 'تم حذف العنوان بنجاح';
+
+  @override
+  String get noAddressFound => 'لم يتم العثور على عناوين';
+
+  @override
+  String get addNewAddress => 'أضف عنوانًا جديدًا للبدء!';
+
+  @override
+  String get addAddress => 'أضف عنوانًا جديدًا';
+
+  @override
+  String get addressAddedSuccessfully => 'تمت إضافة العنوان بنجاح';
+
+  @override
+  String get street => 'شارع';
+
+  @override
+  String get enterYourStreet => 'أدخل شارعك';
+
+  @override
+  String get city => 'مدينة';
+
+  @override
+  String get enterYourCity => 'ادخل مدينتك';
+
+  @override
+  String get aboutUsContent => 'مرحبًا بك في تطبيق Elevate Flower! نحن متخصصون في مساعدتك على الاحتفال بأعز لحظات حياتك من خلال الباقة المثالية. سواء كان عيد ميلاد، أو ذكرى سنوية، أو زفاف، أو أي مناسبة خاصة، فإننا نؤمن بأن كل مناسبة تستحق لمسة من جمال الأزهار. مجموعات الزهور المنسقة بعناية تجعل من السهل التعبير عن الحب والفرح والامتنان. مع الطلب السلس والتسليم في الوقت المناسب، نحن هنا لتحويل كل مناسبة إلى ذكرى دائمة.';
+
+  @override
+  String get termsAndConditionsContent => 'مرحبًا بك في تطبيق Elevate Flower! باستخدام تطبيقنا المحمول، فإنك توافق على هذه الشروط والأحكام. يرجى قراءتها بعناية قبل الوصول إلى خدماتنا أو استخدامها.\n\n1. قبول الشروط\nمن خلال الوصول إلى تطبيق Elevate Flower (\"الخدمة\") أو استخدامه، فإنك توافق على الالتزام بهذه الشروط. إذا كنت لا توافق على هذه الشروط، فلا يجوز لك استخدام خدمتنا.\n\n2. حسابات المستخدم\nإنشاء الحساب: للوصول إلى ميزات معينة، قد يُطلب منك إنشاء حساب. يجب عليك تقديم معلومات دقيقة وكاملة.\nمسؤولية الحساب: أنت مسؤول عن الحفاظ على سرية بيانات اعتماد حسابك وجميع الأنشطة التي تتم تحت حسابك.\nإنهاء الحساب: نحتفظ بالحق في إنهاء الحسابات التي تنتهك هذه الشروط.\n\n3. الطلبات والمدفوعات\nقبول الطلب: تخضع جميع الطلبات للقبول والتوافر. نحتفظ بالحق في إلغاء أو رفض أي طلب وفقًا لتقديرنا.\nالتسعير: الأسعار المدرجة هي بالدولار الأمريكي. نحتفظ بالحق في تحديث الأسعار دون إشعار.\nالدفع: يجب أن يتم الدفع في وقت الطلب من خلال طرق الدفع المدعومة لدينا.\n\n4. سياسة التوصيل\nمناطق التوصيل: نقوم بالتسليم إلى المواقع المحددة في التطبيق. قد تختلف أوقات التسليم حسب الموقع والتوافر.\nالتسليم المتأخر: لا نتحمل مسؤولية التأخير بسبب ظروف خارجة عن إرادتنا (على سبيل المثال، الطقس، العطلات، المشكلات الفنية).\n\n5. سياسة الاسترداد والإلغاء\nالإلغاءات: يمكن إلغاء الطلبات حتى [الإطار الزمني] قبل التسليم المقرر.\n\nالاسترداد: يخضع الاسترداد لسياسة الاسترداد الخاصة بنا، والتي تم تفصيلها في التطبيق.\n\n6. استخدام الخدمة\nأنت توافق على عدم إساءة استخدام الخدمة، بما في ذلك على سبيل المثال لا الحصر:\nانتهاك أي قوانين أو لوائح.\nمحاولة تعطيل أو اختراق الخدمة.\nاستخدام الخدمة لأغراض غير مصرح بها.\n\n7. الملكية الفكرية\nجميع المحتويات الموجودة في التطبيق، بما في ذلك النصوص والصور والشعارات والتصميم، مملوكة لشركة Elevate Flower App أو مرخصيها. لا يجوز لك نسخ أو تعديل أو توزيع أي محتوى دون إذن.\n\n8. الحد من المسؤولية\nإلى الحد الأقصى المسموح به بموجب القانون، لا يتحمل تطبيق Elevate Flower المسؤولية عن أي أضرار مباشرة أو غير مباشرة أو عرضية ناجمة عن استخدامك للخدمة.\n\n9. التغييرات على الشروط\nنحتفظ بالحق في تحديث هذه الشروط في أي وقت. يشكل الاستمرار في استخدام الخدمة بعد التغييرات قبولاً للشروط الجديدة.';
+
+  @override
+  String get chooseLanguage => 'اختر اللغة';
+
+  @override
+  String get arabic => 'العربية';
+
+  @override
+  String get pleaseEnterLogin => 'من فضلك قم بتسجيل الدخول';
+
+  @override
+  String get subTotal => 'المجموع الفرعي';
+
+  @override
+  String get deliveryFee => 'رسوم التوصيل';
+
+  @override
+  String get total => 'الإجمالي';
+
+  @override
+  String get cart => 'سلة';
+
+  @override
+  String get items => 'عنصر';
+
+  @override
+  String get yourCartIsEmpty => 'سلة التسوق الخاصة بك فارغة';
+
+  @override
+  String get addCart => 'إضافة إلى السلة';
+
+  @override
+  String get checkout => 'الدفع';
+
+  @override
+  String get schedule => 'الجدول الزمني';
+
+  @override
+  String get deliveryTime => 'وقت التسليم';
+
+  @override
+  String get instant => 'فوري';
+
+  @override
+  String get deliveryAddress => 'عنوان التسليم';
+
+  @override
+  String get addNew => 'إضافة جديد';
+
+  @override
+  String get paymentMethod => 'طريقة الدفع';
+
+  @override
+  String get cashOnDelivery => 'الدفع عند الاستلام';
+
+  @override
+  String get creditCard => 'بطاقة الائتمان';
+
+  @override
+  String get payment => 'دفع';
+
+  @override
+  String get arriveBy => 'يصل بواسطة';
+
+  @override
+  String get name => 'اسم';
+
+  @override
+  String get phoneNumber => 'رقم الهاتف';
+
+  @override
+  String get enterYourName => 'ادخل اسمك';
+
+  @override
+  String get enterYourPhoneNumber => 'ادخل رقم هاتفك';
+
+  @override
+  String get placeOrder => 'اطلب';
+
+  @override
+  String get itsGift => 'هدية';
+
+  @override
+  String get failedToLoadAddress => 'فشل في تحميل العنوان';
+
+  @override
+  String get notifications => 'الإشعارات';
 }
