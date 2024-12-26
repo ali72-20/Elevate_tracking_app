e0b7b67a29082b9fbf6aba05371f6e6e11e1a974
alisafwat <alisafwat11@gmail.com>
2024-12-26 16:32:12 +0200

    add: confirm otp method to forget password online data source

--- lib/src/data/data_sources/online_data_source/forget_password/forget_password_online_data_source_impl.dart
+++ lib/src/data/data_sources/online_data_source/forget_password/forget_password_online_data_source_impl.dart
@@ -1,6 +1,7 @@
 
 import 'package:injectable/injectable.dart';
 import 'package:tracking_app/src/data/api/api_services.dart';
+import 'package:tracking_app/src/data/api/core/api_request_models/forget_password_request_models/confirm_otp_request_model.dart';
 import 'package:tracking_app/src/data/api/core/api_request_models/forget_password_request_models/get_otp_request_model.dart';
 import 'package:tracking_app/src/data/api/core/api_response_models/forget_password/get_otp_response_model.dart';
 import 'package:tracking_app/src/data/data_sources/online_data_source/forget_password/forget_password_online_data_source.dart';
@@ -13,5 +14,10 @@ class ForgetPasswordOnlineDataSourceImpl implements ForgetPasswordOnlineDataSour
   @override
   Future<GetOtpResponseModel> getOtp(GetOtpRequestModel getOtpRequestModel) async{
     return await _apiServices.getOtp(getOtpRequestModel);
+  }
+
+  @override
+  confirmOtp(ConfirmOtpRequestModel confirmOtpRequestModel) async {
+    return await _apiServices.confirmOtp(confirmOtpRequestModel);
   }
 }
