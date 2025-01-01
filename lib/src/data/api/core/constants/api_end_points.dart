61869f1dc93406457693b5ed22b2772c0d561291
alisafwat <alisafwat11@gmail.com>
2025-01-01 04:01:18 +0200

    add: apply screen end point

--- lib/src/data/api/core/constants/api_end_points.dart
+++ lib/src/data/api/core/constants/api_end_points.dart
@@ -3,4 +3,5 @@ class ApiEndPoints{
   static const String forgetPassword = "forgotPassword";
   static const String verifyResetCode = "verifyResetCode";
   static const String resetPassword = "resetPassword";
+  static const String apply = "apply";
 }
