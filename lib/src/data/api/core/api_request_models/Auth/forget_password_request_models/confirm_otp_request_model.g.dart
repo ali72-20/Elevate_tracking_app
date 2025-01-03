e33f040025e70bb74cb6c1e39adcaee2189639cf
alisafwat <alisafwat11@gmail.com>
2025-01-03 10:39:45 +0200

    add: change password end point add: change password request model add: change password response model add; change password api call add: change password online data source call add: change password domain entity add: chang password use case

---
+++ lib/src/data/api/core/api_request_models/Auth/forget_password_request_models/confirm_otp_request_model.g.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'confirm_otp_request_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+ConfirmOtpRequestModel _$ConfirmOtpRequestModelFromJson(
+        Map<String, dynamic> json) =>
+    ConfirmOtpRequestModel(
+      resetCode: json['resetCode'] as String?,
+    );
+
+Map<String, dynamic> _$ConfirmOtpRequestModelToJson(
+        ConfirmOtpRequestModel instance) =>
+    <String, dynamic>{
+      'resetCode': instance.resetCode,
+    };
